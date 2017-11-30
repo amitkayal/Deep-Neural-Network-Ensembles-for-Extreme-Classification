a0e2a59c9264c8299ba35b229f7b3ae6e9ef3b7c
Yuxuan Sun <yxsun.shawn@gmail.com>
2017-11-30 04:02:33 -0500

    update

--- trainer_inception3_180.py
+++ trainer_inception3_180.py
@@ -145,7 +145,7 @@ def run_training():
 
     #pretrained_file = '/home/ck/project/data/pretrain/inception_v3_google-1a9a5a14.pth'
     #pretrained_file = '/root/share/data/models/pytorch/imagenet/inception/inception_v3_google-1a9a5a14.pth'
-    pretrained_file = None#'../trained_models/LB=0.69565_inc3_00075000_model.pth'
+    pretrained_file = '../trained_models/LB=0.69565_inc3_00075000_model.pth'
     #None#'/home/ck/project/results/inception3-299-02a/checkpoint/00040000_model.pth'
     skip = [] #['fc.weight', 'fc.bias']
 
