d7438f7074827a843901a0edf6195b9a63a14de9
Yuxuan Sun <yxsun.shawn@gmail.com>
2017-11-30 15:40:26 -0500

    update scheduler, add utils

---
+++ utils.py
@@ -0,0 +1,10 @@
+def get_learning_rate(optimizer):
+    lr=[]
+    for param_group in optimizer.param_groups:
+       lr +=[ param_group['lr'] ]
+    return lr
+
+
+def adjust_learning_rate(optimizer, lr):
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
