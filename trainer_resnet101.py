f60adcfc8a6cd4fb3f5ae7412ebd0ad47a3e3f3e
Yuxuan Sun <yxsun.shawn@gmail.com>
2017-11-30 23:00:41 -0500

    update

--- trainer_resnet101.py
+++ trainer_resnet101.py
@@ -230,7 +230,6 @@ def run_training():
     log.write('** dataset setting **\n')
     transform = transforms.Compose([
         # transforms.ToTensor(): Converts a PIL.Image or numpy.ndarray (H x W x C) in the range [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
-        transforms.RandomCrop((160, 160)),
         transforms.Lambda(lambda x:train_augment(x))
 
     ])
