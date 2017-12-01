0d05ec9479e68c5327bedb744f9e76e69e6d89a3
PAN001 <pan.atopos@gmail.com>
2017-11-30 23:03:43 -0500

    update

--- trainer_resnet101.py
+++ trainer_resnet101.py
@@ -166,8 +166,8 @@ def run_training():
 
     validation_num = 10000
 
-    batch_size  = 128 #60   #512  #96 #256
-    validation_batch_size = 128
+    batch_size  = 64 #60   #512  #96 #256
+    validation_batch_size = 64
     iter_accum  = 4 #2  #448//batch_size
 
     valid_loss  = 0.0
