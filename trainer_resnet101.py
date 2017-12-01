5920a1ba81a8d3ac68defcbfb125d10f35593dc5
Yuxuan Sun <yxsun.shawn@gmail.com>
2017-11-30 22:50:15 -0500

    update transform

--- trainer_resnet101.py
+++ trainer_resnet101.py
@@ -241,7 +241,7 @@ def run_training():
     transform = transforms.Compose([
         # transforms.ToTensor(): Converts a PIL.Image or numpy.ndarray (H x W x C) in the range [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
         transforms.Lambda(lambda x:train_augment(x)),
-        transforms.RandomCrop([160,160])
+        transforms.RandomCrop((160,160))
     ])
     train_dataset = CDiscountDataset(csv_dir+train_data_filename,root_dir,transform=transform)
 
