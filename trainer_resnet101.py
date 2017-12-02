4daf5219952545e458b2d06978eb49d4fc066b8f
PAN001 <pan.atopos@gmail.com>
2017-12-02 11:12:38 -0500

    update

--- trainer_resnet101.py
+++ trainer_resnet101.py
@@ -106,6 +106,15 @@ def save_checkpoint(optimizer, i, epoch, net, best_valid_acc, best_train_acc, ou
         'best_train_acc': best_train_acc
     }, out_dir + '/checkpoint/' + IDENTIFIER + "/" + name)
 
+    torch.save({
+        'optimizer': optimizer.state_dict(),
+        'iter': i,
+        'epoch': epoch,
+        'state_dict': net.state_dict(),
+        'best_valid_acc': best_valid_acc,
+        'best_train_acc': best_train_acc
+    }, out_dir + '/checkpoint/' + IDENTIFIER + "/latest.pth")
+
 
 #--------------------------------------------------------------
 def evaluate(net, test_loader, sample_num):
@@ -153,7 +162,7 @@ def run_training():
     #-------------------------------------------- Training settings --------------------------------------------
     out_dir  = '../' # s_xx1'
     # initial_checkpoint = None
-    initial_checkpoint = "../checkpoint/"+ IDENTIFIER + "/00243200_model.pth"
+    initial_checkpoint = "../checkpoint/"+ IDENTIFIER + "/00243300_model.pth"
     # initial_checkpoint = '../trained_models/resnet_00243000_model.pth'
     # pretrained_file = '../trained_models/LB=0.69565_inc3_00075000_model.pth'
     pretrained_file = None
