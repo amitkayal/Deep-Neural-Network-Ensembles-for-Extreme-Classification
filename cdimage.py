e42db35e4347440e73d750202e6b065843bb8292
Yuxuan Sun <yxsun.shawn@gmail.com>
2017-12-02 22:19:23 -0500

    update

--- cdimage.py
+++ cdimage.py
@@ -105,82 +105,9 @@ def pytorch_image_to_tensor_transform(image):
 
     return tensor
 
-def pytorch_tensor_to_image_transform(tensor):
-    mean = [0.485, 0.456, 0.406 ]
-    std  = [0.229, 0.224, 0.225 ]
-
-    tensor[0] = tensor[0]*std[0] + mean[0]
-    tensor[1] = tensor[1]*std[1] + mean[1]
-    tensor[2] = tensor[2]*std[2] + mean[2]
 
 
-    image = tensor.numpy()*255
-    image = np.transpose(image, (1, 2, 0))
-    image = image.astype(np.uint8)
-    image = cv2.cvtColor(image , cv2.COLOR_BGR2RGB)
-    return image
 
 
-def image_to_tensor_transform(image):
-    tensor = pytorch_image_to_tensor_transform(image)
-    tensor[0] = tensor[0] * (0.229 / 0.5) + (0.485 - 0.5) / 0.5
-    tensor[1] = tensor[1] * (0.224 / 0.5) + (0.456 - 0.5) / 0.5
-    tensor[2] = tensor[2] * (0.225 / 0.5) + (0.406 - 0.5) / 0.5
-    return tensor
 
 
-def train_augment(image):
-    if random.random() < 0.5:
-        image = random_shift_scale_rotate(image,
-                                          # shift_limit  = [0, 0],
-                                          shift_limit=[-0.06, 0.06],
-                                          scale_limit=[0.9, 1.2],
-                                          rotate_limit=[-10, 10],
-                                          aspect_limit=[1, 1],
-                                          # size=[1,299],
-                                          borderMode=cv2.BORDER_REFLECT_101, u=1)
-    else:
-        pass
-
-    # flip  random ---------
-    image = random_horizontal_flip(image, u=0.5)
-
-    tensor = image_to_tensor_transform(image)
-    return tensor
-
-# transform = transforms.Compose([
-#                     # transforms.ToTensor(): Converts a PIL.Image or numpy.ndarray (H x W x C) in the range [0, 255] to a torch.FloatTensor of shape (C x H x W) in the range [0.0, 1.0].
-#                     transforms.Lambda(lambda x: pytorch_image_to_tensor_transform(x))
-#                 ])
-# dataset = CDiscountDataset(csv_dir+data_file_name,root_dir,transform=transform)
-# sampler = SequentialSampler(dataset)
-# data_loader= DataLoader(dataset,batch_size=256,shuffle=False,num_workers=0)
-# for i, data in enumerate(data_loader, 0):
-#     images, labels = data
-#     print("image type:",type(images))
-#     print("image content:",images)
-#     print("image numpy:",images[0].numpy())
-#     plt.imshow(images[0].numpy().transpose(1,2,0))
-#     plt.show()
-#     break
-
-# for i, (images, indices) in enumerate(data_loader, 0):
-#
-#     batch_size = len(indices)
-#     num_augments = len(images)
-#     print('batch_size = %d' % batch_size)
-#     print('num_augments = %d' % num_augments)
-#     for a in range(num_augments):
-#         tensor = images[a][0]
-#         print('%d: %s' % (a, str(tensor.size())))
-#         image = pytorch_tensor_to_image_transform(tensor)
-#         plt.imshow('image%d' % a, image)
-#     cv2.waitKey(0)
-#     xx = 0
-
-# for iter, (images, labels) in enumerate(data_loader):
-#     print(type(images))
-#     #print(images)
-#     plt.imshow(images[0].numpy())
-#     plt.show()
-#     break
